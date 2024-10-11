d1f7de2f9ae0933a020e43f4832cdbad4c470d74
Victorkaffekande <victor.gugerel@gmail.com>
2024-10-11 11:43:35 +0200

    generate password

--- static/js/copy.js
+++ static/js/copy.js
@@ -1,6 +1,18 @@
 ﻿function copyToClipboard(elementId) {
-    let copyGfGText = document.getElementById(elementId);
-    let val = copyGfGText.value
+    let element = document.getElementById(elementId);
+    let val = element.value
 
     navigator.clipboard.writeText(val)
+}
+
+function generatePassword(inputId) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    const length = 20
+
+    for (let i = 0, n = charset.length; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    let inputElement = document.getElementById(inputId)
+    inputElement.value = password
 }
